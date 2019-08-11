d526721647bc727b49ae863d559d3a151e92cc41
Shantay Grady <shantaygrady@gmail.com>
2019-08-11 17:05:49 -0400

    Sets up dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
 import './App.css';
 
 function App() {
+  const [state, setState] = useState;
+
+  useEffect(() => {
+    axios
+      .get('https://lambda-coin.herokuapp.com/chain')
+      .then(res => {
+        setState(res.data);
+      })
+      .catch(err => {
+        console.log("You have an error!", err);
+      });
+  }, []);
+  
+  console.log(state);
+  
   return (
     <div className="App">
       welcome
